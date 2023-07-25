45714198a4474365e89f185217630602807f012f
Annie David <E20CSE068@bennett.edu.in>
2023-07-26 01:36:13 +0530

    set up done

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
-
+const mongoose = require("mongoose");
+const cors = require("cors");
 const app = express();
+const { readdirSync } = require("fs");
+const dotenv = require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+//ROUTES
+readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
-app.get("/", (req, res) => {
-  res.send("welcome from home");
-});
-app.get("/books", (req, res) => {
-  res.send("hahahahahahahhahahaaiidhiagduogauodhguagdigaiduygiuagduagdiu");
-});
-app.listen(8000, () => {
+//database
+mongoose
+  .connect(process.env.DATABASE_URL, {
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log("DB Connection Error:", err));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => {
   console.log("server is listening...");
 });
